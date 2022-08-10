75062316d68c6745876bed47f86a5d0548633b09
Dávid Komorowicz <dawars00@gmail.com>
2022-08-10 06:08:16 +0200

    Inference all splits

--- inference.py
+++ inference.py
@@ -1,13 +1,18 @@
 import argparse
 
 import torchvision
+from PIL import Image
 from matplotlib import pyplot as plt
 from pytorch_lightning import Trainer
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, ConcatDataset
 from torchvision.transforms import Compose
 
 from datasets.labelmefacade import LabelMeFacade
 from model import DeepLab
+import os
+
+OUT_PATH = '/mnt/hdd/datasets/facade/output/etrims/test'
+os.makedirs(OUT_PATH, exist_ok=True)
 
 
 def get_argparser():
@@ -15,8 +20,11 @@ def get_argparser():
     parser.add_argument('--backbone', type=str, default='resnet101',
                         choices=['resnet101', 'mobilenet'], help='backbone name')
     parser.add_argument('--ckpt_path', type=str,
-                        default='/mnt/hdd/datasets/facade/experiments/deeplab/ckpts/deeplabv3plus_resnet101_labelmefacade/trial_14/epoch=79.ckpt',
+                        default='/mnt/hdd/datasets/facade/experiments/deeplab/ckpts/deeplabv3plus_resnet101_labelmefacade_best_hyper/trial_0/epoch=79.ckpt',
                         help='path of ckpt to load')
+
+#    '/mnt/hdd/datasets/facade/experiments/deeplab/ckpts/deeplabv3plus_resnet101_labelmefacade_best_hyper/trial_0/epoch=79.ckpt' if ckpt_mode == 'hand' else \
+#    '/mnt/hdd/datasets/facade/experiments/deeplab/ckpts/deeplabv3plus_resnet101_labelmefacade_batchsize2/trial_24/epoch=64.ckpt'  # best ce
 
     return parser.parse_args()
 
@@ -33,14 +41,26 @@ def main():
                                          std=[0.229, 0.224, 0.225]),
     ])
 
-    dataset = LabelMeFacade('/mnt/hdd/datasets/facade/labelmefacade', split='test', transform=val_transform)
-    dataloader = DataLoader(dataset, batch_size=len(dataset), shuffle=False, num_workers=4, pin_memory=True, drop_last=False)
+    datasets = []
+    for iteration in range(3):
+        split_file = f"/mnt/hdd/datasets/facade/bootstrapping/split_{iteration}.txt"
+        dataset = LabelMeFacade('/mnt/hdd/datasets/facade/ZuBuD/ZuBuD/png-ZuBuD', 'test', transform=val_transform,
+                            split_file=split_file, img_dir='', ext='.png')
+        datasets.append(dataset)
+    dataset_inference = ConcatDataset(datasets)
+
+    dataloader = DataLoader(dataset_inference, batch_size=len(dataset_inference), shuffle=False, num_workers=4,
+                            pin_memory=True,
+                            drop_last=False)
+
     predictions = trainer.predict(model, dataloaders=dataloader)
     for batch in predictions:
-        for pred in batch:
-            mask = dataset.decode_target(pred).astype("uint")
-            plt.imshow(mask)
-            plt.show()
+        for i in range(len(batch[1])):
+            pred = batch[0][i]
+            name = batch[1][i]
+            mask = datasets[0].decode_target(pred).astype("uint8")
+
+            Image.fromarray(mask).save(os.path.join(OUT_PATH, f"{name}.png"))
 
     # decode
     print(len(predictions))
